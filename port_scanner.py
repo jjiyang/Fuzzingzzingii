9f038003a39f0735dfe3e2703b634e4cf866ab75
wxuycea <numbbvi@naver.com>
2024-07-13 13:13:12 +0900

    Fix: update code

---
+++ port_scanner.py
@@ -0,0 +1,37 @@
+import requests
+from concurrent.futures import ThreadPoolExecutor
+from urllib.parse import urlparse, urlunparse
+
+
+class PORTSCANNER:
+    def __init__(self):
+        with open('./payloads/SSRF/port.txt', 'r') as f:
+            self.port_num = [port.strip() for port in f]
+
+    def requesting(self, url):
+        if requests.get(url, timeout=2).status_code == 200:
+            print(f'Port Scanner :\t{url} open!!')
+        else:
+            print(f'Port Scanner :\t{url} close!!')
+
+    def port_scan(self, url, param):
+        url_list = []
+        for port in self.port_num:
+            parsed_param = urlparse(param)
+            domain = parsed_param.netloc
+            domain = domain.replace(f':{parsed_param.port}', '') if parsed_param.port else domain
+
+            if domain != "":
+                param_domain = domain + f':{port}'
+                port_param = parsed_param._replace(netloc=param_domain)
+                port_param = urlunparse(port_param)
+                port_url = url+port_param
+                url_list.append(port_url)
+
+        with ThreadPoolExecutor(max_workers=5) as executor:
+            list(executor.map(self.requesting, url_list))
+
+
+# 포트 스캔 사용법
+# 사용자가 의심 가는 url 및 파라미터가 있다면
+# 입력하도록
